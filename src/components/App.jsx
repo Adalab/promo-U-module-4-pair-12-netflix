6f30fab12a58b80d6885c4b3be2fe91b9e678d3f
Ainhoadlhs <ainhoasilveira@gmail.com>
2023-10-30 08:05:01 +0100

    add react

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+//imports dependencias, imagenes, componentes, stylos
+import '../styles/App.scss'
+
+
+function App() {
+
+//funciones, variables, handles, 
+//html
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
